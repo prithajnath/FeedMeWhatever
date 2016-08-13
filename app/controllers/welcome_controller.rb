dcb451ff620e94a1804fbe51d51d653ff5e5d681
Prithaj <prithajnath@gmail.com>
2016-08-13 15:36:00 +0000

    Fetch tags from picture

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @something = ClarifaiRuby::TagRequest.new.get("https://samples.clarifai.com/metro-north.jpg")
+    @something = ClarifaiRuby::TagRequest.new.get("http://res.cloudinary.com/dzmp7xptn/image/upload/v1471102345/taco_lww6md.jpg")
   end
 end
