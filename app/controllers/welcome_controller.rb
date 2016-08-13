c539e4c883ea4f6531a579a7dadc7ab1075aeb6f
Prithaj <prithajnath@gmail.com>
2016-08-13 17:56:38 +0000

    Add food model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @something = ClarifaiRuby::TagRequest.new.get("http://res.cloudinary.com/dzmp7xptn/image/upload/v1471102345/taco_lww6md.jpg")
+    @something = ClarifaiRuby::TagRequest.new.get("http://res.cloudinary.com/dzmp7xptn/image/upload/v1471102345/taco_lww6md.jpg", model: "food-items-v0.1")
   end
 end
