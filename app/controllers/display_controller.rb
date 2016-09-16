3b76edc7d08a80a305078b01bc27fce16dca1231
Prithaj <prithajnath@gmail.com>
2016-09-16 22:57:20 +0000

    Add encoding for image URL

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -1,7 +1,9 @@
 class DisplayController < ApplicationController
 
+  require 'base64'
+
   def show
-    @food = ClarifaiRuby::TagRequest.new.get(Image.find_by(params[:id]).pic.url, model: "food-items-v0.1")
+    @food = ClarifaiRuby::TagRequest.new.get(Base64.encode64(Image.find_by(params[:id]).pic.url), model: "food-items-v0.1")
   end
 
 end
