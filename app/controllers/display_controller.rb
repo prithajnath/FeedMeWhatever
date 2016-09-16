fcaa2b5390d1609de2eaa76ecd4a5fb70d7e1429
Prithaj <prithajnath@gmail.com>
2016-09-16 05:25:55 +0000

    Link uploaded images with the Clarifai API

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -1,7 +1,7 @@
 class DisplayController < ApplicationController
 
   def show
-    @food = ClarifaiRuby::TagRequest.new.get(Picture.find(params[:id]).url.to_s, model: "food-items-v0.1")
+    @food = ClarifaiRuby::TagRequest.new.get(Image.find_by(params[:id]).pic.url, model: "food-items-v0.1")
   end
 
 end
